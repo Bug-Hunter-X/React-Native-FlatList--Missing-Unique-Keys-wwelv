dd885c7bf4a9caade0c56e3bf9c3510151652e6e
BugHunter-X <askzenai@gmail.com>
2025-01-27 12:06:06 +0100

    Add FixedFlatList.js file

---
+++ FixedFlatList.js
@@ -0,0 +1,26 @@
+```javascript
+import React from 'react';
+import { FlatList, Text, View } from 'react-native';
+
+const data = [
+  { id: '1', title: 'Item 1' },
+  { id: '2', title: 'Item 2' },
+  { id: '3', title: 'Item 3' },
+];
+
+const FixedFlatList = () => {
+  return (
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <View>
+          <Text>{item.title}</Text>
+        </View>
+      )}
+    />
+  );
+};
+
+export default FixedFlatList;
+```
